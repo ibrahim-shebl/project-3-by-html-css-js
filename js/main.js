80356fa888a0056dd2b35ca80f108e12f745c349
ibrahim-shebl <117544216+ibrahim-shebl@users.noreply.github.com>
2023-04-26 14:42:19 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let bar = document.getElementById("bar");
+let close = document.getElementById("close");
+let navbar = document.getElementById("navbar");
+
+if(bar){
+    bar.addEventListener("click", () => {
+        navbar.classList.add("active");
+    });
+}
+
+if(close){
+    close.addEventListener("click", () => {
+        navbar.classList.remove("active");
+    });
+}
